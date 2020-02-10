b2fc9f9b46cdb0208bd68011089d672ac12cd74a
Oguzhan <oguzhan.ercedogan@edu-kw1c.nl>
2020-02-10 15:16:53 +0100

    Add Index.php

---
+++ Hoofdstuk 2/Index.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * User: Name
+ * Date: 0-0-0000
+ * Time: 00:00
+ * File: Index.php
+ */
+?>
+<html>
+    <head>
+        <!-- Tekenset declareren -->
+        <meta charset="utf-8">
+        <!-- Omschrijving van de webpagina -->
+        <meta name="discription" content="Opdracht X">
+        <!-- Toekennen van sleutelwoorden -->
+        <meta name="keywords" content="keyword 1, keyword 2">
+        <!-- Geeft de link aan van je CSS bestand -->
+        <link href="../CSS/style.css" rel="stylesheet" type="text/css">
+        <!-- geeft titel aan -->
+        <title>
+            Template
+        </title>
+    </head>
+    <body>
+        <header>
+            <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+        </header>
+        <hr>
+        <aside>
+            <h2>Menu</h2>
+            <h3>Hoofdstuk 2</h3>
+            <ul>
+                <li>
+                    <a href="opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="opdracht2.2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+            <h3>Hoofdstuk 3</h3>
+            <ul>
+                <li>
+                    <a href="">Opdracht 3.1</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 3.2</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 3.3</a>
+                </li>
+            </ul>
+            <h3>Hoofdstuk 4</h3>
+            <ul>
+                <li>
+                    <a href="">Opdracht 4.1</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 4.2</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 4.3</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 4.4</a>
+                </li>
+            </ul>
+            <h3>Hoofdstuk 5</h3>
+            <ul>
+                <li>
+                    <a href="">Opdracht 5.1</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 5.2</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 5.3</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 5.4</a>
+                </li>
+            </ul>
+        </aside>
+        <main id="wrapper">
+            <h2>Uitwerkingen</h2>
+        </main>
+    </body>
+</html>
