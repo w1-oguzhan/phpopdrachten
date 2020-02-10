53f78df61c3836e8fb5b79d0c74e32b2fd8a159b
Oguzhan <oguzhan.ercedogan@edu-kw1c.nl>
2020-02-10 20:33:40 +0100

    Add style.css

--- Hoofdstuk 2/Index.php
+++ Hoofdstuk 2/Index.php
@@ -15,7 +15,7 @@
         <!-- Toekennen van sleutelwoorden -->
         <meta name="keywords" content="keyword 1, keyword 2">
         <!-- Geeft de link aan van je CSS bestand -->
-        <link href="../CSS/style.css" rel="stylesheet" type="text/css">
+        <link href="CSS/style.css" rel="stylesheet" type="text/css">
         <!-- geeft titel aan -->
         <title>
             Template
@@ -31,10 +31,10 @@
             <h3>Hoofdstuk 2</h3>
             <ul>
                 <li>
-                    <a href="opdracht2.1.php">Opdracht 2.1</a>
+                    <a href="">Opdracht 2.1</a>
                 </li>
                 <li>
-                    <a href="opdracht2.2.php">Opdracht 2.2</a>
+                    <a href="">Opdracht 2.2</a>
                 </li>
             </ul>
             <h3>Hoofdstuk 3</h3>
