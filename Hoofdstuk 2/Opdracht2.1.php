a2f40874bce78f23213c09232fe3d2d997e5cfbe
Oguzhan <oguzhan.ercedogan@edu-kw1c.nl>
2020-02-19 14:10:27 +0100

    Add helloworld.php

--- Hoofdstuk 2/Opdracht2.1.php
+++ Hoofdstuk 2/Opdracht2.1.php
@@ -1,89 +1,15 @@
 <?php
-/**
- * User: Name
- * Date: 0-0-0000
- * Time: 00:00
- * File: Index.php
- */
+//$base_url = "http://".$_SERVER['SERVER_NAME'].dirname($_SERVER["REQUEST_URI"].'?').'/';
+$base_url = "http://localhost/phpopdrachten/";
 ?>
-<html>
-    <head>
-        <!-- Tekenset declareren -->
-        <meta charset="utf-8">
-        <!-- Omschrijving van de webpagina -->
-        <meta name="discription" content="Opdracht X">
-        <!-- Toekennen van sleutelwoorden -->
-        <meta name="keywords" content="keyword 1, keyword 2">
-        <!-- Geeft de link aan van je CSS bestand -->
-        <link href="../CSS/style.css" rel="stylesheet" type="text/css">
-        <!-- geeft titel aan -->
-        <title>
-            Template
-        </title>
-    </head>
-    <body>
-        <header>
-            <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
-        </header>
-        <hr>
-        <aside>
-            <h2>Menu</h2>
-            <br>
-            <h3>Hoofdstuk 2</h3>
-            <ul>
-                <li>
-                    <a href="Hoofdstuk%202/Opdracht2.1.php.php">Opdracht 2.1</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 2.2</a>
-                </li>
-            </ul>
-            <h3>Hoofdstuk 3</h3>
-            <ul>
-                <li>
-                    <a href="">Opdracht 3.1</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 3.2</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 3.3</a>
-                </li>
-            </ul>
-            <h3>Hoofdstuk 4</h3>
-            <ul>
-                <li>
-                    <a href="">Opdracht 4.1</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 4.2</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 4.3</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 4.4</a>
-                </li>
-            </ul>
-            <h3>Hoofdstuk 5</h3>
-            <ul>
-                <li>
-                    <a href="">Opdracht 5.1</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 5.2</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 5.3</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 5.4</a>
-                </li>
-            </ul>
-        </aside>
-        <main id="wrapper">
-            <h2 id="out">Uitwerkingen</h2>
-            <p><?php echo "Hello world" ?></p>
-        </main>
-    </body>
-</html>
+<?php
+include "../Includes/header.php";
+include "../Includes/menu.php";
+include "../Includes/footer.php";
+?>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+    <?php
+    echo "hello world"
+    ?>
+</main>
--- Includes/menu.php
+++ Includes/menu.php
@@ -1,10 +1,15 @@
+<?php
+//$base_url = "http://".$_SERVER['SERVER_NAME'].dirname($_SERVER["REQUEST_URI"].'?').'/';
+$base_url = "http://localhost/phpopdrachten/";
+?>
 <aside>
     <h2>Menu</h2>
     <br>
     <h3>Hoofdstuk 2</h3>
     <ul>
         <li>
-            <a href="Hoofdstuk%202/Opdracht2.1.php">Opdracht 2.1</a>
+            <a href="<?php echo $base_url;?>Hoofdstuk 2/Opdracht2.1.php">Opdracht 2.1</a>
+<!--            <a href="Hoofdstuk%202/Opdracht2.1.php">Opdracht 2.1</a>-->
         </li>
         <li>
             <a href="">Opdracht 2.2</a>
