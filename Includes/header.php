f644b1699d5bdb23b15e5795d4ca78215ed0cc42
Oguzhan <oguzhan.ercedogan@edu-kw1c.nl>
2020-02-19 13:47:20 +0100

    Add Includes

---
+++ Includes/header.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User: Name
+ * Date: 0-0-0000
+ * Time: 00:00
+ * File: Index.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Tekenset declareren -->
+    <meta charset="utf-8">
+    <!-- Omschrijving van de webpagina -->
+    <meta name="discription" content="Opdracht X">
+    <!-- Toekennen van sleutelwoorden -->
+    <meta name="keywords" content="keyword 1, keyword 2">
+    <!-- Geeft de link aan van je CSS bestand -->
+    <link href="/phpopdrachten/CSS/style.css" rel="stylesheet" type="text/css">
+    <!-- geeft titel aan -->
+    <title>
+        Template
+    </title>
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<hr>
---
+++ Includes/menu.php
@@ -0,0 +1,55 @@
+<aside>
+    <h2>Menu</h2>
+    <br>
+    <h3>Hoofdstuk 2</h3>
+    <ul>
+        <li>
+            <a href="Hoofdstuk%202/Opdracht2.1.php">Opdracht 2.1</a>
+        </li>
+        <li>
+            <a href="">Opdracht 2.2</a>
+        </li>
+    </ul>
+    <h3>Hoofdstuk 3</h3>
+    <ul>
+        <li>
+            <a href="">Opdracht 3.1</a>
+        </li>
+        <li>
+            <a href="">Opdracht 3.2</a>
+        </li>
+        <li>
+            <a href="">Opdracht 3.3</a>
+        </li>
+    </ul>
+    <h3>Hoofdstuk 4</h3>
+    <ul>
+        <li>
+            <a href="">Opdracht 4.1</a>
+        </li>
+        <li>
+            <a href="">Opdracht 4.2</a>
+        </li>
+        <li>
+            <a href="">Opdracht 4.3</a>
+        </li>
+        <li>
+            <a href="">Opdracht 4.4</a>
+        </li>
+    </ul>
+    <h3>Hoofdstuk 5</h3>
+    <ul>
+        <li>
+            <a href="">Opdracht 5.1</a>
+        </li>
+        <li>
+            <a href="">Opdracht 5.2</a>
+        </li>
+        <li>
+            <a href="">Opdracht 5.3</a>
+        </li>
+        <li>
+            <a href="">Opdracht 5.4</a>
+        </li>
+    </ul>
+</aside>
